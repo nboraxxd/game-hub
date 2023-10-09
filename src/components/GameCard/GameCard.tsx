3c3ce6a94e3b25f71b8d445d8bc18fc7b8464dc9
bora <nhatbaoxxd@gmail.com>
2023-10-09 12:34:49 +0700

    Get optimized images

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react'
 import { Game } from '@/types/games.type'
 import { PlatformIconList } from '@/components/PlatformIconList'
 import { CriticScore } from '@/components/CriticScore'
+import getCroppedImageUrl from '@/config/image-url'
 
 interface Props {
   game: Game
@@ -10,7 +11,7 @@ interface Props {
 export default function GameCard({ game }: Props) {
   return (
     <Card overflow="hidden" borderRadius={10}>
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justify="space-between">
