5c6861e5924781b6fa9103eba7c4c4747b72184c
bora <nhatbaoxxd@gmail.com>
2023-10-11 10:03:11 +0700

    Build the home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,35 @@
+import { Box, HStack, Show } from '@chakra-ui/react'
+import { GenreList } from '@/components/GenreList'
+import { GameGrid } from '@/components/GameGrid'
+
 export default function HomePage() {
-  return <div>HomePage</div>
+  return (
+    <HStack alignItems="start">
+      <Show above="lg">
+        <Box
+          as="aside"
+          w="200px"
+          flexShrink="0"
+          pos="sticky"
+          top="0"
+          height="100vh"
+          overflow="auto"
+          css={{
+            '&::-webkit-scrollbar': {
+              height: 'var(--chakra-sizes-1)',
+              width: 'var(--chakra-sizes-1)',
+            },
+            '&::-webkit-scrollbar-thumb': {
+              backgroundColor: 'var(--chakra-colors-gray-400)',
+            },
+          }}
+        >
+          <GenreList />
+        </Box>
+      </Show>
+      <Box as="main" flexGrow="1">
+        <GameGrid />
+      </Box>
+    </HStack>
+  )
 }
