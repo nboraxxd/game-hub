560d2cb34b004529c2022195370e447aa909e786
bora <nhatbaoxxd@gmail.com>
2023-10-11 11:04:36 +0700

    Build the not found page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,3 +1,53 @@
+import { Link } from 'react-router-dom'
+import { Box, Button, Flex, Heading, Text } from '@chakra-ui/react'
+import { PATH } from '@/config'
+import background from '@/assets/images/background-not-found-page.jpg'
+
 export default function NotFound() {
-  return <div>NotFound</div>
+  return (
+    <Flex
+      as="section"
+      pos="relative"
+      alignItems="center"
+      justifyContent="center"
+      w="100%"
+      h="100vh"
+      pb="100"
+      backgroundImage={background}
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover"
+      _before={{
+        content: `""`,
+        position: 'absolute',
+        left: '0',
+        top: '0',
+        width: '100%',
+        height: '100%',
+        bgGradient: 'linear(to-r, black, blackAlpha.900)',
+        opacity: '0.9',
+      }}
+    >
+      <Box pos="relative" px="4" color="white" textAlign="center" fontSize="3xl">
+        <Heading as="h1" fontSize="12rem" fontWeight="semibold" noOfLines={1}>
+          404
+        </Heading>
+        <Text>We couldn't find that page.</Text>
+        <Button
+          as={Link}
+          height="3.125rem"
+          maxWidth="19rem"
+          width="full"
+          mt="8"
+          backgroundColor="white"
+          _hover={{ backgroundColor: 'gray.100' }}
+          color="#333"
+          fontSize="lg"
+          fontWeight="semibold"
+          to={PATH.homePage}
+        >
+          Main page
+        </Button>
+      </Box>
+    </Flex>
+  )
 }
--- src/router/routers.tsx
+++ src/router/routers.tsx
@@ -4,15 +4,6 @@ import { HomePage } from '@/pages/HomePage'
 import { NotFound } from '@/pages/NotFound'
 
 export const routers = [
-  {
-    path: PATH.homePage,
-    element: <MainLayout />,
-    children: [
-      { index: true, element: <HomePage /> },
-      {
-        path: PATH.notFound,
-        element: <NotFound />,
-      },
-    ],
-  },
+  { path: PATH.homePage, element: <MainLayout />, children: [{ index: true, element: <HomePage /> }] },
+  { path: PATH.notFound, element: <NotFound /> },
 ]
