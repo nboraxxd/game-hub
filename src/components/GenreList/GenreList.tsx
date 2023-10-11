0995365a9851a994fb5820358c0969d7f6a31074
bora <nhatbaoxxd@gmail.com>
2023-10-11 23:20:34 +0700

    Show skeleton while fetching the genres

--- src/components/GenreList/GenreList.tsx
+++ src/components/GenreList/GenreList.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import { Button, Heading, Image, List, ListItem, Text, useColorModeValue } from '@chakra-ui/react'
+import { Button, Heading, Image, List, ListItem, Skeleton, Text, useColorModeValue } from '@chakra-ui/react'
 import { Genre } from '@/types/genres.type'
-import { getCroppedImageUrl } from '@/config'
-import useFetch from '@/hooks/useFetch'
+import { SERVICE_STATUS, getCroppedImageUrl } from '@/config'
 import { genresService } from '@/services/genres.service'
+import useFetch from '@/hooks/useFetch'
 import { icons } from '@/utils'
 
 const INITIAL_END_GENRE_INDEX = 9
@@ -14,8 +14,11 @@ export default function GenreList() {
   )
   const isAllGenres = endGenreIndex === undefined
   const IconButton = isAllGenres ? icons.up : icons.down
+  const buttonColor = useColorModeValue('gray.600', 'gray.400')
+  const buttonColorHover = useColorModeValue('gray.800', 'gray.200')
 
-  const { data } = useFetch<Genre>(genresService.getGenres)
+  const { data: genres, status, error } = useFetch<Genre>(genresService.getGenres)
+  const isLoadingGenres = status === (SERVICE_STATUS.idle || SERVICE_STATUS.pending)
 
   function handleToggle() {
     setEndGenreIndex((endGenreIndex) =>
@@ -23,45 +26,57 @@ export default function GenreList() {
     )
   }
 
+  if (error) return null
+
   return (
     <>
       <Heading as="h2" fontSize="2xl">
         Genres
       </Heading>
       <List mt={4}>
-        {data.slice(0, endGenreIndex).map((genre) => (
-          <ListItem key={genre.id} display="flex" alignItems="center" mt={3}>
-            <Image
-              src={getCroppedImageUrl(genre.image_background)}
-              alt={genre.name}
-              boxSize="32px"
-              borderRadius="8"
-              objectFit="cover"
-            />
-            <Text ml={3}>{genre.name}</Text>
-          </ListItem>
-        ))}
-        <ListItem mt={3} mb={10}>
-          <Button
-            leftIcon={<IconButton size="1.5rem" />}
-            variant="unstyled"
-            onClick={handleToggle}
-            display="flex"
-            alignItems="center"
-            justifyContent="start"
-            w="full"
-            h={8}
-            color={useColorModeValue('gray.600', 'gray.400')}
-            _hover={{ color: useColorModeValue('gray.800', 'gray.200') }}
-            sx={{
-              '& .chakra-button__icon': {
-                marginRight: '12px',
-              },
-            }}
-          >
-            {isAllGenres ? 'Hide' : 'Show all'}
-          </Button>
-        </ListItem>
+        {isLoadingGenres ? (
+          Array.from(Array(10)).map((_, index) => (
+            <ListItem key={index} mt={3}>
+              <Skeleton h="32px" maxW="full" />
+            </ListItem>
+          ))
+        ) : (
+          <>
+            {genres.slice(0, endGenreIndex).map((genre) => (
+              <ListItem key={genre.id} display="flex" alignItems="center" mt={3}>
+                <Image
+                  src={getCroppedImageUrl(genre.image_background)}
+                  alt={genre.name}
+                  boxSize="32px"
+                  borderRadius="8"
+                  objectFit="cover"
+                />
+                <Text ml={3}>{genre.name}</Text>
+              </ListItem>
+            ))}
+            <ListItem mt={3} mb={10}>
+              <Button
+                leftIcon={<IconButton size="1.5rem" />}
+                variant="unstyled"
+                onClick={handleToggle}
+                display="flex"
+                alignItems="center"
+                justifyContent="start"
+                w="full"
+                h={8}
+                color={buttonColor}
+                _hover={{ color: buttonColorHover }}
+                sx={{
+                  '& .chakra-button__icon': {
+                    marginRight: '12px',
+                  },
+                }}
+              >
+                {isAllGenres ? 'Hide' : 'Show all'}
+              </Button>
+            </ListItem>
+          </>
+        )}
       </List>
     </>
   )
