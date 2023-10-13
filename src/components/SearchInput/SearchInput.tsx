59fd29e49dd9d6cea5ab3a4c58bb4f9d15153bc1
bora <nhatbaoxxd@gmail.com>
2023-10-14 00:19:23 +0700

    Use useState to manage searchValue

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -3,17 +3,26 @@ import { Icon, Input, InputGroup, InputLeftElement, InputRightElement, Kbd, Show
 import { useHotkeys } from 'react-hotkeys-hook'
 
 import { icons } from '@/utils'
-import { FormEvent, useRef, useState } from 'react'
+import { FormEvent, useState, useRef } from 'react'
 import useSearchParamsObj from '@/hooks/useSearchParamsObj'
 import { PATH } from '@/config'
 import { GamesConfig } from '@/types'
+import useDisUpdateEffect from '../../hooks/useDisUpdateEffect'
 
 export default function SearchInput() {
   const navigate = useNavigate()
   const paramsObj: GamesConfig = useSearchParamsObj()
 
   const inputRef = useRef<HTMLInputElement>(null)
+  const [searchValue, setSearchValue] = useState<string>(paramsObj.search || '')
   const [isFocused, setIsFocused] = useState(false)
+  useDisUpdateEffect(() => {
+    if (!paramsObj.search) {
+      setSearchValue('')
+    } else {
+      setSearchValue(paramsObj.search)
+    }
+  }, [paramsObj.search || ''])
 
   useHotkeys('ctrl+k', () => (isFocused ? inputRef.current?.blur() : inputRef.current?.focus()), {
     ignoreModifiers: true,
@@ -21,25 +30,16 @@ export default function SearchInput() {
     enableOnFormTags: ['input'],
   })
 
-  useHotkeys(
-    'esc',
-    () => {
-      if (isFocused) {
-        inputRef.current?.blur()
-      }
-    },
-    {
-      enableOnFormTags: ['input'],
-    }
-  )
+  useHotkeys('esc', () => inputRef.current?.blur(), {
+    enableOnFormTags: ['input'],
+  })
 
-  function handleSubmit(ev: FormEvent<HTMLDivElement>) {
+  function handleSubmit(ev: FormEvent<HTMLInputElement>) {
     ev.preventDefault()
 
-    const value = inputRef.current?.value.trim()
-    const search = value ? { ...paramsObj, search: value } : paramsObj
-
+    const search = searchValue.trim() ? { ...paramsObj, search: searchValue.trim() } : paramsObj
     const searchParams = createSearchParams(search).toString()
+
     navigate({
       pathname: PATH.homePage,
       search: searchParams,
@@ -52,13 +52,14 @@ export default function SearchInput() {
         <Icon as={icons.search} />
       </InputLeftElement>
       <Input
-        defaultValue={paramsObj.search}
         ref={inputRef}
+        value={searchValue}
+        onChange={(ev) => setSearchValue(ev.target.value)}
         borderRadius={20}
         placeholder="Search games..."
         variant="filled"
         pr={{ base: 'none', lg: '5rem' }}
-        textOverflow="ellipsis"
+        _placeholderShown={{ textOverflow: 'ellipsis' }}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
       />
