6b957d169f9d4237e1e851fcda4d61833e66b754
bora <nhatbaoxxd@gmail.com>
2023-10-17 04:41:16 +0700

    Implement lazy load

--- src/router/routers.tsx
+++ src/router/routers.tsx
@@ -1,7 +1,10 @@
+/* eslint-disable react-refresh/only-export-components */
+import { lazy } from 'react'
 import { PATH } from '@/config'
 import { MainLayout } from '@/layouts/MainLayout'
-import { HomePage } from '@/pages/HomePage'
-import { NotFound } from '@/pages/NotFound'
+
+const HomePage = lazy(() => import('@/pages/HomePage/HomePage'))
+const NotFound = lazy(() => import('@/pages/NotFound/NotFound'))
 
 export const routers = [
   { path: PATH.homePage, element: <MainLayout />, children: [{ index: true, element: <HomePage /> }] },
