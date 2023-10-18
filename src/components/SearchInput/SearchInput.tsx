91a84435ed1d6f5dc670bb2a661103a4b6294a0c
bora <nhatbaoxxd@gmail.com>
2023-10-18 10:36:35 +0700

    Bugs: Replace ctrl+k with control+k in useHotkeys

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useState, useRef } from 'react'
 import { createSearchParams, useNavigate } from 'react-router-dom'
 import { Icon, Input, InputGroup, InputLeftElement, InputRightElement, Kbd, Show } from '@chakra-ui/react'
 import { useHotkeys } from 'react-hotkeys-hook'
+import { isMacOs } from 'react-device-detect'
 
 import { GamesConfig } from '@/types'
 import useDisUpdateEffect from '@/hooks/useDisUpdateEffect'
@@ -10,12 +11,15 @@ import { icons } from '@/utils'
 import { PATH } from '@/config'
 
 export default function SearchInput() {
+  const controlK = isMacOs ? 'Control+K' : 'Ctrl+K'
+
   const navigate = useNavigate()
   const paramsObj: GamesConfig = useSearchParamsObj()
 
   const inputRef = useRef<HTMLInputElement>(null)
   const [searchValue, setSearchValue] = useState<string>(paramsObj.search || '')
   const [isFocused, setIsFocused] = useState(false)
+
   useDisUpdateEffect(() => {
     if (!paramsObj.search) {
       setSearchValue('')
@@ -24,7 +28,7 @@ export default function SearchInput() {
     }
   }, [paramsObj.search || ''])
 
-  useHotkeys('ctrl+k', () => (isFocused ? inputRef.current?.blur() : inputRef.current?.focus()), {
+  useHotkeys('control+k', () => (isFocused ? inputRef.current?.blur() : inputRef.current?.focus()), {
     ignoreModifiers: true,
     preventDefault: true,
     enableOnFormTags: ['input'],
@@ -36,6 +40,9 @@ export default function SearchInput() {
 
   function handleSubmit(ev: FormEvent<HTMLInputElement>) {
     ev.preventDefault()
+    if (searchValue.trim() === paramsObj.search) {
+      return inputRef.current?.blur()
+    }
 
     const search = searchValue.trim() ? { ...paramsObj, search: searchValue.trim() } : paramsObj
     const searchParams = createSearchParams(search).toString()
@@ -67,7 +74,7 @@ export default function SearchInput() {
       />
       <Show above="lg">
         <InputRightElement w="5rem" gap={1} pointerEvents="none">
-          {isFocused ? <Kbd>Enter</Kbd> : <Kbd>Ctrl+K</Kbd>}
+          <Kbd>{isFocused ? 'Enter' : controlK}</Kbd>
         </InputRightElement>
       </Show>
     </InputGroup>
